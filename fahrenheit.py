7c32d665b01619f697c869506711f4029df4c3c0
pabbu <avocado20031208@gmail.com>
2025-08-02 16:58:25 +0900

    화씨변환기

---
+++ fahrenheit.py
@@ -0,0 +1,12 @@
+def convert(tmp1):
+
+    Fahrenheit = ((9/5) * tmp1) + 32
+    
+    return Fahrenheit
+
+print('본 프로그램은 섭씨를 화씨로 변환해주는 프로그램입니다.')
+
+tmp = float(input('변환하고 싶은 섭씨 온도를 입력해주세요: '))
+
+print('섭씨온도: ' , tmp)
+print('화씨온도: ', f'{convert(tmp): .2f}')
